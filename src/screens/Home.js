61813732138e27b99a3b4f78c0ed85a98eaa9f53
Tiffany Barclay <tiffany@barnet.co.nz>
2020-09-27 19:02:01 +1300

    Display product component on Home page

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import Product from "../components/Product";
 
 function Home() {
   return (
@@ -11,15 +12,49 @@ function Home() {
           alt="Amazon Exports"
         ></img>
         <div className="home-row">
-          {/* Product */}
-          {/* Product */}
+          <Product
+            name="The Lean Startup: How Constant Innovation Creates Radically Successful
+          Businesses Paperback"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            rating={3}
+          />
+          <Product
+            name="Kenwood Kmix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+            price={239.0}
+            image="https://images-na.ssl-images-amazon.com/images/I/41hbQSkaNxL._AC_.jpg"
+            rating={4}
+          />
         </div>
         <div className="home-row">
-          {/* Product */}
-          {/* Product */}
-          {/* Product */}
+          <Product
+            name="Zevro Zero Gravity Magnetic Spice Rack with 12 Canisters"
+            price={43.89}
+            image="https://images-na.ssl-images-amazon.com/images/I/61gWcQJp3BL._AC_SX679_.jpg"
+            rating={4}
+          />
+
+          <Product
+            name="Revlon One-Step Hair Dryer And Volumizer Hot Air Brush, Black, Packaging May Vary"
+            price={41.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/71aXzv34N%2BL._SX425_.jpg"
+            rating={2}
+          />
+          <Product
+            name="Oculus Quest All-in-one VR Gaming Headset – 128GB"
+            price={499.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/71D9OsZmWxL._SX522_.jpg"
+            rating={5}
+          />
         </div>
-        <div className="home-row">{/* Product */}</div>
+        <div className="home-row">
+          <Product
+            name="AOC CQ34G2 Super Curved Frameless Gaming Monitor, UltraWide FHD 2560x1080, 1500R VA Panel, 1ms MPRT, 75Hz, FreeSync, Height Adjustable, 3-Yr Zero Dead Pixels"
+            price={309.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/41%2BCXFgQwjL._AC_.jpg"
+            rating={4}
+          />
+        </div>
       </div>
     </div>
   );
