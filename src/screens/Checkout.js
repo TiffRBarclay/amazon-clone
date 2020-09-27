af4f7b5c40ec87015d33e72bf1fdc1906a833499
Tiffany Barclay <tiffany@barnet.co.nz>
2020-09-27 19:10:19 +1300

    Add checkout component content

--- src/screens/Checkout.js
+++ src/screens/Checkout.js
@@ -4,7 +4,25 @@ import "./Checkout.css";
 function Checkout() {
   return (
     <div className="checkout">
-      <h1>Checkout Screen</h1>
+      <div className="checkout-left">
+        <img
+          className="checkout-ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt="amazon-ad"
+        />
+        <div>
+          <h2 className="checkout-title">Your Shopping Basket</h2>
+          {/* Bakset Item */}
+          {/* Bakset Item */}
+          {/* Bakset Item */}
+          {/* Bakset Item */}
+          {/* Bakset Item */}
+        </div>
+      </div>
+
+      <div className="checkout-right">
+        <h2>The Subtotal Section</h2>
+      </div>
     </div>
   );
 }
