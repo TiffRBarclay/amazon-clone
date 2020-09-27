8c471763349f8fb9e3ceef503d02a52e98525a9d
Tiffany Barclay <tiffany@barnet.co.nz>
2020-09-27 19:36:11 +1300

    Add react context API

---
+++ src/ReactContextAPI/StateProvider.js
@@ -0,0 +1,14 @@
+import React, { createContext, useContext, useReducer } from "react";
+
+// Prepares Data Layer
+export const StateContext = createContext();
+
+// Wrap out Application and Provide Access to the Data Layer
+export const StateProvider = ({ reducer, initialState, children }) => (
+  <StateContext.Provider value={useReducer(reducer, initialState)}>
+    {children}
+  </StateContext.Provider>
+);
+
+// Pull Information from the Data Layer
+export const useStateValues = () => useContext(StateContext);
