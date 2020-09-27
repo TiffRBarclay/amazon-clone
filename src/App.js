7b3404f8da645fe5230a32c1734e855862e4a04a
Tiffany Barclay <tiffany@barnet.co.nz>
2020-09-27 19:00:15 +1300

    Wrap Application in the Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+
 import Header from "./components/Header";
 import Home from "./screens/Home";
+import Checkout from "./screens/Checkout";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
-      <Home />
-    </div>
+      <Switch>
+        <Route path="/checkout">
+          <Checkout />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
