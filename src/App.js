2d6739b202a47c46337358ed01977ce69ba302b7
Tiffany Barclay <tiffany@barnet.co.nz>
2020-09-28 14:15:42 +1300

    Add Stripe higher class and authentication to App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
@@ -6,13 +6,53 @@ import Header from "./components/Header";
 import Home from "./screens/Home";
 import Checkout from "./screens/Checkout";
 import Login from "./screens/Login";
+import Payment from "./screens/Payment";
+
+import { auth } from "./firebase/Firebase";
+import { useStateValue } from "./ReactContextAPI/StateProvider";
+
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51HWA4wHSIitAiqqolr4GPyhR1DCjMxSpprD276uuBmMOuYDgAnwlwS7C9BOHD4YX0Esa2QaThkqkuSZ2vO9J0YOg006dY44ggq"
+);
 
 function App() {
+  const [{ user }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // Event Listener
+    auth.onAuthStateChanged((authUser) => {
+      console.log("The user is >>> ", authUser);
+
+      if (authUser) {
+        // user logged in || user already logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // user logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
     <Router>
       <Switch>
         <Route path="/login">
           <Login />
+        </Route>
+        <Route path="/payment">
+          <Header />
+          <Elements stripe={promise}>
+            <Payment />
+          </Elements>
         </Route>
         <Route path="/checkout">
           <Header />
